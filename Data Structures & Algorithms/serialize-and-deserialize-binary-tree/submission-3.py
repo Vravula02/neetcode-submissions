20b8190e7d822ee86c7f9042072452a33f7ebf7c
Vravula02 <varshithreddy.ravula02@gmail.com>
2026-07-30 19:50:34 -0400

    Add: serialize-and-deserialize-binary-tree - submission-3

---
+++ Data Structures & Algorithms/serialize-and-deserialize-binary-tree/submission-3.py
@@ -0,0 +1,66 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Codec:
+    
+    # Encodes a tree to a single string.
+    def serialize(self, root: Optional[TreeNode]) -> str:
+
+        if not root:
+            return ""
+        
+        res=[]
+
+        dq=collections.deque([root])
+
+        while dq:
+
+            node=dq.popleft()
+
+            if node:
+                res.append(str(node.val))
+                dq.append(node.left)
+                dq.append(node.right)
+        
+            else:
+                res.append("#")
+
+        return ",".join(res)
+        
+    # Decodes your encoded data to tree.
+    def deserialize(self, data: str) -> Optional[TreeNode]:
+
+        if data=="":
+            return None
+        
+        vals=data.split(',')
+        root=TreeNode(int(vals[0]))
+        dq=collections.deque([root])
+        ind=1
+
+        while dq :
+
+            node=dq.popleft()
+
+            if vals[ind]!="#":
+                node.left=TreeNode(int(vals[ind]))
+                dq.append(node.left)
+            ind+=1
+
+            if vals[ind]!="#":
+                node.right=TreeNode(int(vals[ind]))
+                dq.append(node.right)
+            ind+=1
+        
+        return root
+
+
+
+
+            
+            
+
