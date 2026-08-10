424dece28643d64fbc67a694533e7ee86a3129ad
Vravula02 <varshithreddy.ravula02@gmail.com>
2026-08-10 14:57:53 -0400

    Add: rotting-fruit - submission-0

---
+++ Data Structures & Algorithms/rotting-fruit/submission-0.py
@@ -0,0 +1,38 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+
+        dq=collections.deque()
+
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if grid[row][col]==2:
+                    dq.append((row,col,0))
+        Time=0
+        while dq:
+
+            for _ in range(len(dq)):
+                row,col,time=dq.popleft()
+                Time=max(time,Time)
+
+                for dr,dc in [(1,0),(-1,0),(0,-1),(0,1)]:
+                    i,j=row+dr,col+dc
+
+                    if self.isValid(i,j,grid) and grid[i][j]==1:
+                        grid[i][j]=2
+                        dq.append((i,j,time+1))
+        
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if grid[row][col]==1:
+                    return -1
+        return Time
+    
+    def isValid(self,row,col,grid):
+
+        if row<0 or col<0 or row>=len(grid) or col>=len(grid[0]):
+            return False
+        return True
+                
+
+
+        
