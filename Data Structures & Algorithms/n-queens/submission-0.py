e69c087e9501047aa34abe6bed236723419ae652
Vravula02 <varshithreddy.ravula02@gmail.com>
2026-08-13 16:50:10 -0400

    Add: n-queens - submission-0

---
+++ Data Structures & Algorithms/n-queens/submission-0.py
@@ -0,0 +1,48 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+
+        res=[]
+        board=[["." for _ in range(n)] for _ in range(n)]
+        self.placeQueen(0,0,board,res)
+        return res
+    
+    def placeQueen(self,row,col,board,res):
+
+        if col==len(board[0]):
+            res.append(["".join(row) for row in board])
+            return 
+
+        for i in range(len(board)):
+            if self.isValid(i,col,board):
+                board[i][col]="Q"
+                self.placeQueen(i,col+1,board,res)
+                board[i][col]="."
+            
+
+    def isValid(self,row,col,board):
+
+        for i in range(col):
+            if board[row][i]=="Q":
+                return False
+        
+        r=row-1
+        c=col-1
+        while r>=0 and c>=0:
+            if board[r][c]=="Q":
+                return False
+            r-=1
+            c-=1
+        
+        r=row+1
+        c=col-1
+
+        while c>=0 and r<len(board):
+            if board[r][c]=="Q":
+                return False
+            c-=1
+            r+=1
+        return True
+
+
+
+        
