e3a267c418f887e9f9421ee8ab9229ec64b9728b
Vravula02 <varshithreddy.ravula02@gmail.com>
2026-08-26 14:33:35 -0400

    Add: foreign-dictionary - submission-1

---
+++ Data Structures & Algorithms/foreign-dictionary/submission-1.py
@@ -0,0 +1,66 @@
+class Solution:
+    def foreignDictionary(self, words: List[str]) -> str:
+
+        graph=collections.defaultdict(set)
+
+        for word in words:
+            for ch in word:
+                graph[ord(ch)-ord('a')]
+        
+        for i in range(1,len(words)):
+            word1=words[i-1]
+            word2=words[i]
+
+            minLen=min(len(word1),len(word2))
+
+            if len(word1)>len(word2) and word1[:minLen]==word2:
+                return ""
+            
+            for j in range(minLen):
+                if word1[j]!=word2[j]:
+                    graph[ord(word1[j])-ord('a')].add(ord(word2[j])-ord('a'))
+                    break
+        
+        topo=self.topoSort(graph)
+
+        if len(topo)!=len(graph):
+            return ""
+        
+        res=[]
+
+        for node in topo:
+            res.append(chr(node+ord('a')))
+        return "".join(res)
+    
+    def topoSort(self,graph):
+
+        indegree=[0]*26
+
+        for node in graph.keys():
+            for neighbor in graph[node]:
+                indegree[neighbor]+=1
+        
+        dq=collections.deque()
+
+        for node in graph:
+            if indegree[node]==0:
+                dq.append(node)
+        
+        topo=[]
+
+        while dq:
+
+            node=dq.popleft()
+            topo.append(node)
+
+            for neighbor in graph[node]:
+                indegree[neighbor]-=1
+                if indegree[neighbor]==0:
+                    dq.append(neighbor)
+        return topo
+
+        
+            
+            
+
+        
